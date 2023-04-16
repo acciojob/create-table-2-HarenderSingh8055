56868141608b5c691f94ba116cdf00529c39b97a
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-04-16 12:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function createTable() {
     //Write your code here
 
     let rowNum = prompt("Input number of rows");
-    let colNum = prompt("Input number of rows");
+    let colNum = prompt("Input number of columns");
     let table = document.getElementById("myTable");
 
     console.log(rowNum , colNum);
